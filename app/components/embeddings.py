3f204950c519ca3f62db043f766226981957834a
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-07-29 19:24:36 +0400

    add HuggingFace embedding model loader with logging and error handling

---
+++ app/components/embeddings.py
@@ -0,0 +1,23 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from config.config import HF_EMBEDDINGS_MODEL
+
+from app.common.logger import get_logger
+from app.common.custom_exception import CustomException
+
+logger = get_logger(__name__)
+
+
+def get_embeddings_model():
+    """
+    Get the embeddings model from HuggingFace.
+    """
+    try:
+        logger.info(f"Initializing embedding model: {HF_EMBEDDINGS_MODEL}")
+        model = HuggingFaceEmbeddings(model_name=HF_EMBEDDINGS_MODEL)
+        logger.info(f"Embedding model loaded successfully")
+        return model
+    
+    except Exception as e:
+        error_msg= CustomException(f"Failed to load embedding model", e)
+        logger.error(str(error_msg))  
+        raise error_msg  

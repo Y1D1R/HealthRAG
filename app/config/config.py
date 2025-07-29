bf06659b117f1c4b543f702c1fe4a1afcb7f97e1
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-07-29 19:24:55 +0400

    add HuggingFace embedding model

--- app/config/config.py
+++ app/config/config.py
@@ -6,6 +6,9 @@ HF_TOKEN = os.environ.get("HF_TOKEN")
 # LLM model repo ID
 HF_REPO_ID = "mistralai/Mistral-7B-Instruct-v0.3"
 
+# Embeddings model
+HF_EMBEDDINGS_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
+
 # FAISS vector DB location
 DB_FAISS_PATH = "vectorstore/db_faiss"
 

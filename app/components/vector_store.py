da38331eb09d0e85d717091b02eef9b8bf5501a2
Y1D1R <koulalyidhiraghiles@gmail.com>
2025-07-29 22:01:23 +0400

    reduce FAISS log verbosity

--- app/components/vector_store.py
+++ app/components/vector_store.py
@@ -5,6 +5,8 @@ from app.components.embeddings import get_embeddings_model
 from app.common.logger import get_logger
 from app.common.custom_exception import CustomException
 from app.config.config import DB_FAISS_PATH
+import logging
+logging.getLogger("faiss").setLevel(logging.WARNING)
 
 logger = get_logger(__name__)
 
